1f115e40b4c9540a4cab7a85150d7b474a026a12
asteroidfive <skyytles99@gmail.com>
2021-10-08 16:06:31 -0400

    v1

--- instagram/AppDelegate.swift
+++ instagram/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let parseConfig = ParseClientConfiguration {
+                $0.applicationId = "3EDbSvx8TyHldPmDWMhKCavJ45lPvxv6AO4vqfq4"
+                $0.clientKey = "Pnt4fFqnXnflxZs1JrtuLAplQYX8sOFDwzDJaTmt"
+                $0.server = "https://parseapi.back4app.com"
+        }
+        Parse.initialize(with: parseConfig)
+
+        
         return true
     }
 
@@ -30,7 +40,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
+    
 
 }
 
